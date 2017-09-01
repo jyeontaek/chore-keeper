06161bdeb6ea1176c12fa782e7748e919aa1dce6
Yeon Taek Jeong <yeontaekj@gmail.com>
2017-08-31 17:01:32 -0700

    Update Chore model

--- app/src/main/java/com/yeontaekj/chorekeeper/Chore.java
+++ app/src/main/java/com/yeontaekj/chorekeeper/Chore.java
@@ -1,5 +1,7 @@
 package com.yeontaekj.chorekeeper;
 
+import java.util.UUID;
+
 /**
  * Created by yeontaekj on 8/24/2017.
  */
@@ -7,12 +9,30 @@ package com.yeontaekj.chorekeeper;
 public class Chore {
 
     private String name;
+    private String description;
+    private UUID mUUID;
+
+    public Chore() {
+        this(null, null);
+    }
 
     public Chore(String name) {
+        this(name, null);
+    }
+
+    public Chore(String name, String description) {
         this.name = name;
+        this.description = description;
+        mUUID = UUID.randomUUID();
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getDescription() { return description;}
+
+    public String getUUID() {
+        return mUUID.toString();
     }
 }
