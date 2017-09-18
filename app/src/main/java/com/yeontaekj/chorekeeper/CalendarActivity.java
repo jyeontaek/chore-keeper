ec8c940e0ae585fed82e672e9486e0e7957467a7
Yeon Taek Jeong <yeontaekj@gmail.com>
2017-09-18 16:33:46 -0700

    Change CalendarView widget to MaterialCalendarView

--- app/src/main/java/com/yeontaekj/chorekeeper/CalendarActivity.java
+++ app/src/main/java/com/yeontaekj/chorekeeper/CalendarActivity.java
@@ -4,15 +4,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CalendarView;
 
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+
+import java.util.List;
+
 public class CalendarActivity extends AppCompatActivity {
 
-    private CalendarView mCalendarView;
+    private MaterialCalendarView mCalendarView;
+    private Chore chore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
 
-        mCalendarView = (CalendarView) findViewById(R.id.calendar_view);
+        mCalendarView = (MaterialCalendarView) findViewById(R.id.calendar_view);
+        chore = (Chore) getIntent().getSerializableExtra("chore");
+
+        loadData();
+    }
+
+    //Colors the correct calendar dates when the chore was accomplished.
+    private void loadData() {
+        /*
+        List<DateTime> dateList = chore.getDates();
+        for (DateTime date : dateList) {
+
+        }
+        */
     }
 }
